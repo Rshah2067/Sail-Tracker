8c3d08e7f0e5141f788e853e3f60fe5a99572c5a
Rshah2067 <49097684+Rshah2067@users.noreply.github.com>
2023-10-31 16:04:45 -0400

    NIghtly Commit
    started writing code to collect GPS data

--- src/heaversine.h
+++ src/heaversine.h
@@ -14,6 +14,18 @@ class heaversine {
             return square(cos(theta/2));
         }
     public:
+        heaversine(){
+
+        }
+        heaversine(double initlat, double initlong){
+            initialLat = initlat;
+            initialLong = initlong;
+        }
+        //method to set initial position if a reset is needed
+        void setInitial(double initlat, double initlong){
+            initialLat = initlat;
+            initialLong = initlong;
+        }
         //calculate the distance from a lat and long to the intial lat long
         //based on haversine formula where phi is lat and lambda is long
         double distance(double lng, double lat){
@@ -22,14 +34,14 @@ class heaversine {
         return dist;
         }
         //Calculate the x displacement from initial position in meters
-        double deltaX (double dist){
-            double x = arcsin;
-            return x;
-        }
-        double deltaY (double dist){
-            double y;
-            return y;
-        }
+        // double deltaX (double dist){
+        //     double x = arcsin;
+        //     return x;
+        // }
+        // double deltaY (double dist){
+        //     double y;
+        //     return y;
+        // }
 
 
 
--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,10 @@
 #include <Adafruit_GPS.h>
 #include <SD.h>
 #include <SoftwareSerial.h>
-
+#include <heaversine.h>
 SoftwareSerial gpsSerial(7,6);
 Adafruit_GPS GPS(&gpsSerial);
-
+heaversine hv();
 void setup() {
   Serial.begin(115200);
   delay(500);
@@ -14,20 +14,20 @@ void setup() {
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
   delay(1000);
+   while(!GPS.fix){
+    Serial.println("waiting for fix");
+  }
+  Serial.println(GPS.fixquality);
+  delay(1000);
 }
 
 void loop() {
-  if( GPS.fix){
-  Serial.print("Lattitude");
-  Serial.println(GPS.latitude);
-  
-  Serial.println("Longitude");
-  Serial.println(GPS.longitude);
-}
-else{
+ 
 
-  Serial.println(GPS.fix);
+  if(!GPS.fix){
+     // Serial.println("Lost Print");
+  }
 }
-}
+
 
 // put function definitions here:
