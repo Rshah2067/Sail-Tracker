86fe687dd9706bc8b02cdcba7e0f4a7cf1f4a605
Rshah2067 <49097684+Rshah2067@users.noreply.github.com>
2023-11-01 22:34:26 -0400

    Nightly Commit

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,17 @@
 #include <SD.h>
 #include <SoftwareSerial.h>
 #include <heaversine.h>
+
+//GPS should be connected to digital pins 6&7
 SoftwareSerial gpsSerial(7,6);
 Adafruit_GPS GPS(&gpsSerial);
+//default constructor is used to the reference lat/long is set to 0 by default, must call
 heaversine hv();
+
+Sd2Card card;
+SdVolume volume;
+SdFile root;
+
 void setup() {
   Serial.begin(115200);
   delay(500);
@@ -20,14 +28,21 @@ void setup() {
   Serial.println(GPS.fixquality);
   delay(1000);
 }
-
+//Varaible to store the current GPS position with item 0 being lat and 1 being long
+double posGPS(2);
 void loop() {
- 
+  //TODO implement data logging
+  
 
   if(!GPS.fix){
-     // Serial.println("Lost Print");
+      Serial.println("Lost Print");
+      while(!GPS.fix){
+        Serial.println("waiting to regain fix");
+      }
   }
+  //TODO Implement function that converts minutes and seconds to one lat/long value
+  
+
 }
 
 
-// put function definitions here:
