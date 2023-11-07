c422bffa8dec1a7443c9984b856cb5ae1b003401
Rshah2067 <49097684+Rshah2067@users.noreply.github.com>
2023-11-07 14:23:06 -0500

    Implementing Logging
    Having issues where with the antenna gps struggles to get lock

--- src/main.cpp
+++ src/main.cpp
@@ -3,43 +3,69 @@
 #include <Adafruit_GPS.h>
 #include <SD.h>
 #include <SoftwareSerial.h>
-#include <heaversine.h>
-
+#include <SPI.h>
 //GPS should be connected to digital pins 6&7
 SoftwareSerial gpsSerial(7,6);
 Adafruit_GPS GPS(&gpsSerial);
 //default constructor is used to the reference lat/long is set to 0 by default, must call
-heaversine hv();
+//heaversine hv();
 
-Sd2Card card;
-SdVolume volume;
-SdFile root;
-
+#define GPSECHO = true;
 void setup() {
   Serial.begin(115200);
+  while(!Serial){
+    ;
+  }
   delay(500);
   GPS.begin(9600);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
-  delay(1000);
-   while(!GPS.fix){
-    Serial.println("waiting for fix");
+  GPS.sendCommand(PGCMD_ANTENNA);
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(10))
+  {
+    Serial.println("failed init");
+
+    while(1){
+  //    Serial.println("failed to init");
+    }
+  }
+  
+  Serial.println("SD Card inited");
+  File dataFile = SD.open("Log1.txt",FILE_WRITE);
+  if(dataFile){
+  dataFile.println("Time,lattitude,longitude");
+  Serial.println("Time,lattitude,longitude");
+  dataFile.close();
+  }
+  while(!GPS.fix){
+   
+   Serial.println(GPS.fix);
   }
   Serial.println(GPS.fixquality);
-  delay(1000);
 }
 //Varaible to store the current GPS position with item 0 being lat and 1 being long
-double posGPS(2);
+//double posGPS(2);
 void loop() {
   //TODO implement data logging
+  //String output = "";
   
-
+  File dataFile = SD.open("Log1.txt",FILE_WRITE);
+  if(dataFile){
+    dataFile.println(GPS.latitude_fixed + "," + GPS.longitude_fixed);
+    Serial.println(GPS.latitude_fixed + "," + GPS.longitude_fixed);
+    dataFile.close();
+  }
+  delay(2000);
   if(!GPS.fix){
       Serial.println("Lost Print");
       while(!GPS.fix){
         Serial.println("waiting to regain fix");
       }
   }
+  
   //TODO Implement function that converts minutes and seconds to one lat/long value
   
 
