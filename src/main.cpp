80ebc5f2d5d82c70c288247b859c4d33490540a5
Rshah2067 <49097684+Rshah2067@users.noreply.github.com>
2023-11-08 12:47:53 -0500

    Reverted to Old code and properly included parse statements
    Found out that I need to manually call a parse command for new data to work

--- src/main.cpp
+++ src/main.cpp
@@ -5,24 +5,40 @@
 #include <SoftwareSerial.h>
 #include <SPI.h>
 //GPS should be connected to digital pins 6&7
+<<<<<<< Updated upstream
 SoftwareSerial mySerial(8, 7);
 Adafruit_GPS GPS(&mySerial);
+=======
+SoftwareSerial gpsSerial(8,7);
+Adafruit_GPS GPS(&gpsSerial);
+>>>>>>> Stashed changes
 //default constructor is used to the reference lat/long is set to 0 by default, must call
 //heaversine hv();
 
 void setup() {
   Serial.begin(115200);
+<<<<<<< Updated upstream
  
+=======
+  delay(5000);
+  Serial.println("file");
+  delay(500);
+>>>>>>> Stashed changes
   GPS.begin(9600);
   delay(500);
   GPS.sendCommand(PMTK_SET_NMEA_OUTPUT_RMCGGA);
 
   GPS.sendCommand(PMTK_SET_NMEA_UPDATE_1HZ);
+<<<<<<< Updated upstream
  
   delay(1000);
   mySerial.println(PMTK_Q_RELEASE);
  
 
+=======
+  GPS.sendCommand(PGCMD_ANTENNA);
+  delay(1000);
+>>>>>>> Stashed changes
   Serial.print("Initializing SD card...");
 
   if (!SD.begin(10))
